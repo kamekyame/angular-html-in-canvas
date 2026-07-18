11dc9b0093a18fa63794e811e7abb8dafbba2805
kamekyame <kamekyame@outlook.com>
2026-07-18 16:34:14 +0900

    🚿 不要な記述削除

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HtmlInCanvas } from "./html-in-canvas/html-in-canvas";
+import { HtmlInCanvas } from './html-in-canvas/html-in-canvas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HtmlInCanvas],
+  imports: [HtmlInCanvas],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-html-in-canvas');
